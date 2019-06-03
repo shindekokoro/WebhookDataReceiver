c2ade119fa5a3d403880616db710322797d6e949
shindekokoro <44656807+shindekokoro@users.noreply.github.com>
2019-06-03 14:00:28 -0600

    PvP.js get_size form

--- modules/embeds/pvp.js
+++ modules/embeds/pvp.js
@@ -80,7 +80,7 @@ module.exports.run = async (MAIN, target, sighting, internal_value, time_now, ma
     // DETERMINE HEIGHT, WEIGHT AND SIZE
     pokemon.height = Math.floor(sighting.height*100)/100;
     pokemon.weight = Math.floor(sighting.weight*100)/100;
-    pokemon.size = MAIN.Get_Size(sighting.pokemon_id, sighting.height, sighting.weight);
+    pokemon.size = MAIN.Get_Size(sighting.pokemon_id, sighting.form, sighting.height, sighting.weight);
 
     pokemon.attack = sighting.individual_attack;
     pokemon.defense = sighting.individual_defense;
